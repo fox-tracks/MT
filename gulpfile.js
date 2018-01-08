16187f7d294b7b1d81bb7521bb58a5ce26f33700
fox-tracks <t.andriyanova@yahoo.com>
2018-01-08 22:45:50 +0300

    done gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,34 @@ var sass = require('gulp-sass');
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var minify = require("gulp-csso"); 
+var minify = require("gulp-csso");
 var rename = require("gulp-rename");
+// var server = require("browser-sync").create();
+var htmlmin = require("gulp-htmlmin");
+var del = require("del");
 
-gulp.task('sass', function () {
+gulp.task('sass', ["clean"], function () {
     gulp.src("src/sass/style.scss")
         .pipe(plumber())
         .pipe(sass())
         .pipe(postcss([
             autoprefixer()
-          ]))
+        ]))
         .pipe(gulp.dest("dist/css"))
         .pipe(minify())
         .pipe(rename("style.min.css"))
         .pipe(gulp.dest("dist/css"));
+    // .pipe(server.stream());
 });
+
+gulp.task("html", ["clean"],function () {
+    return gulp.src("*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("clean", function () {
+    return del("dist");
+});
+
+gulp.task("build", ['sass', 'html']);
