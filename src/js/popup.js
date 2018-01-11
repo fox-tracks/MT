261f95301aaa099f7db2c8e7215daa976ee75010
fox-tracks <t.andriyanova@yahoo.com>
2018-01-11 20:51:28 +0300

    done js popup

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,9 +1,20 @@
-'use strict';
 
 var popup = document.querySelector('.popup');
 var playBtn = document.querySelector('.play-btn');
 var closeBtn = popup.querySelector('.popup__close');
-var checkbox = popup.querySelector('agreement__checkbox');
+var checkbox = popup.querySelector('.agreement__checkbox');
+var  proceedBtn = popup.querySelector('.agreement__btn');
+
+checkbox.addEventListener('click', function() {
+  if (checkbox.checked === true) {
+    proceedBtn.removeAttribute('disabled');
+    proceedBtn.addEventListener('click', function() {
+      window.open('https://yandex.ru');
+    });
+  } else {
+    proceedBtn.setAttribute('disabled', 'disabled');
+  }
+});
 
 playBtn.addEventListener('click', function (e) {
   e.preventDefault();
@@ -17,7 +28,7 @@ closeBtn.addEventListener('click', function (e) {
 
 document.addEventListener("keydown", function (e) {
   if (e.target !== popup) {
-      popup.classList.remove("popup--shown");
-      checkbox.removeAttribute('checked');
+    popup.classList.remove("popup--shown");
+    checkbox.removeAttribute('checked');
   }
 });
